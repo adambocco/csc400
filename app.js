338cddb9ff025c7daf8820f3bad4bb129f39da1f
Adam Bocco <adam.bocco@gmail.com>
2021-04-09 11:37:54 -0400

    Upload

--- app.js
+++ app.js
@@ -20,13 +20,14 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(compression());
 app.use(helmet());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
